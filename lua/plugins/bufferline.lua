a0c731eb5a0abc4c20f4862456825bf93c6f3c5c
Thomas Chardin Benouali <thomasbenouali@Host-001.lan>
2025-05-30 23:08:46 +0200

    ajout bufferline

---
+++ lua/plugins/bufferline.lua
@@ -0,0 +1,28 @@
+return {
+    "akinsho/bufferline.nvim",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    version = "*",
+    opts = {
+        options = {
+            mode = "tabs",
+            separator_style = "slant",
+            show_buffer_close_icons = false,
+            show_close_icon = false,
+            enforce_regular_tabs = true,
+            always_show_bufferline = true,
+            diagnostics = "nvim_lsp",
+            diagnostics_indicator = function(count, level)
+                local icon = level:match("error") and "" or ""
+                return " " .. icon .. count
+            end,
+            offsets = {
+                {
+                    filetype = "NvimTree",
+                    text = "File Explorer",
+                    highlight = "Directory",
+                    separator = true,
+                },
+            },
+        },
+    },
+}
