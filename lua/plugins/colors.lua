397794f14baa64a901d71b94f858a1338a0caee4
Thomas Chardin Benouali <thomasbenouali.ansc@hotmail.com>
2025-07-11 17:35:46 +0200

    ajout raccourcis pour déplacer lignes/blocs, changement de thème pour catppuccin, ajout raccourci pour git reset hunk

--- lua/plugins/colors.lua
+++ lua/plugins/colors.lua
@@ -20,12 +20,38 @@ end
 --    },
 --}
 
+--return {
+--    {
+--        "shaunsingh/moonlight.nvim",
+--        config = function()
+--            vim.g.moonlight_transparent = true
+--            vim.cmd.colorscheme("moonlight")
+--            enable_transparency()
+--        end,
+--    },
+--    {
+--        "nvim-lualine/lualine.nvim",
+--        dependencies = {
+--            "nvim-tree/nvim-web-devicons",
+--        },
+--        opts = {
+--            theme = "moonlight",
+--        },
+--    },
+--}
+
 return {
     {
-        "shaunsingh/moonlight.nvim",
+        "catppuccin/nvim",
+        name = "catppuccin",
+        priority = 1000,
         config = function()
-            vim.g.moonlight_transparent = true
-            vim.cmd.colorscheme("moonlight")
+            require("catppuccin").setup({
+                flavour = "macchiato", -- latte, frappe, macchiato, mocha
+                transparent_background = true, -- équivalent à moonlight_transparent
+            })
+            vim.cmd.colorscheme("catppuccin")
+            vim.api.nvim_set_hl(0, "@string.python", { fg = "#c974e0" })
             enable_transparency()
         end,
     },
@@ -35,7 +61,7 @@ return {
             "nvim-tree/nvim-web-devicons",
         },
         opts = {
-            theme = "moonlight",
+            theme = "catppuccin",
         },
     },
 }
--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -5,13 +5,14 @@ return {
             current_line_blame = true,
             current_line_blame_opts = {
                 virt_text = true,
-                virt_text_pos = 'eol', -- 'eol' = fin de ligne
-                delay = 0,  -- aucun délai d’affichage
+                virt_text_pos = "eol", -- 'eol' = fin de ligne
+                delay = 0, -- aucun délai d’affichage
                 ignore_whitespace = false,
             },
-            current_line_blame_formatter = '<author>, <author_time:%Y-%m-%d> - <summary>',
+            current_line_blame_formatter = "<author>, <author_time:%Y-%m-%d> - <summary>",
         })
 
         vim.keymap.set("n", "<leader>gp", "<cmd>Gitsigns preview_hunk<CR>", { desc = "Show a preview hunk" })
-    end
+        vim.keymap.set("n", "<leader>gu", "<cmd>Gitsigns reset_hunk<CR>", { desc = "Undo/reset hunk" })
+    end,
 }
