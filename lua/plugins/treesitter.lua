f5e87a9f37ffbb066abe33ed97fe3dede0df8a88
Thomas Chardin Benouali <thomasbenouali.ansc@hotmail.com>
2025-07-16 23:19:46 +0200

    ajout ensure_installed markdown et markdown inline pour le raccourci K

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -2,23 +2,25 @@ return {
     "nvim-treesitter/nvim-treesitter",
     build = ":TSUpdate",
     config = function()
-	local configs = require("nvim-treesitter.configs")
-	configs.setup({
-	    highlight = { enable = true },
-	    indent = { enable = true },
-	    autotage = { enable = true },
-	    ensure_installed = {
-		"lua",
-		"python",
-		"sql",
-		"yaml",
-		"git_config",
-		"git_rebase",
-		"gitcommit",
-		"gitattributes",
-		"json"
-	    },
-	    auto_install = false,
-	})
-    end
+        local configs = require("nvim-treesitter.configs")
+        configs.setup({
+            highlight = { enable = true },
+            indent = { enable = true },
+            autotage = { enable = true },
+            ensure_installed = {
+                "lua",
+                "python",
+                "sql",
+                "yaml",
+                "git_config",
+                "git_rebase",
+                "gitcommit",
+                "gitattributes",
+                "json",
+                "markdown",
+                "markdown_inline",
+            },
+            auto_install = false,
+        })
+    end,
 }
