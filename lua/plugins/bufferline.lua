51bc0a63ae4843c90181d5f6ded4d5198b8cdd86
Thomas Chardin Benouali <thomasbenouali.ansc@hotmail.com>
2025-09-10 11:45:58 +0200

    change tabs separator style

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -5,7 +5,7 @@ return {
     opts = {
         options = {
             mode = "tabs",
-            separator_style = "slant",
+            separator_style = "thin",
             show_buffer_close_icons = false,
             show_close_icon = false,
             enforce_regular_tabs = true,
